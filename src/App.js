b3cc0de60449cddb11eb6d350894ca736c5fb3c5
colemilne54 <colemilne54@gmail.com>
2023-03-20 14:13:27 -0600

    2 files added, 2 deleted

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import { useState, React } from 'react';
+import { Button } from '@mui/material';
+import './style.css';
+
+export default function App() {
+  const [art, getArt] = useState([]);
+
+  function getRandomArt() {
+    let timeStamp = Math.floor(Date.now() / 1000);
+    let artworkRequest = {
+      resources: 'artworks',
+      fields: [
+        'id',
+        'title',
+        'artist_display',
+        'image_id',
+        'date_display',
+        'medium_display',
+      ],
+      boost: false,
+      limit: 1,
+      query: {
+        function_score: {
+          query: {
+            constant_score: {
+              filter: {
+                exists: {
+                  field: 'image_id',
+                },
+              },
+            },
+          },
+          boost_mode: 'replace',
+          random_score: {
+            field: 'id',
+            seed: timeStamp,
+          },
+        },
+      },
+    };
+
+    // Query Art Insititue of Chicago API and return response
+  }
+
+  const handleSubmit = (event) => {
+    fetch('https://api.artic.edu/api/v1/search', {
+      method: 'POST',
+      body: JSON.stringify(artworkRequest),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        getArt(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        console.log(ageUrl);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ marginTop: 15 }}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
